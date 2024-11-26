bb376156ee3cd0162438ccea57e1fff2c06ab607
Oscar Pagani <paganio@yahoo.com>
2024-11-26 14:48:12 -0800

    added lab 02-openai

--- 02-openai-lab/start/index.js
+++ 02-openai-lab/start/index.js
@@ -6,6 +6,24 @@ const openai = new OpenAI();
 // Ask for 3 interview questions for a React developer
 // Extra Credit: Make this a template to make this dynamic for any kind of dev
 
-async function jobInterview() {}
+async function jobInterview(language) {
+  const stream = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You will be asked to create job interview questions.",
+      },
+      {
+        role: "user",
+        content: `Give me 3 interview questions for a ${language} developer`,
+      },
+    ],
+    model: "gpt-4o",
+    stream: true,
+  });
+  for await (const chunk of stream) {
+    process.stdout.write(chunk.choices[0].delta.content || "");
+  }
+}
 
-jobInterview();
+jobInterview("JavaScript");
